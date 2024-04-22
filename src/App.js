e89884f1bd03287571d77a16c29c2e99f88df43f
Joni Salmi <joni.salmi@supercell.com>
2024-04-22 15:49:13 +0300

    React-widgets V4 DropdownList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import DropdownList from "react-widgets/lib/DropdownList";
+import "react-widgets/dist/css/react-widgets.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DropdownList
+        data={["Red", "Yellow", "Blue", "Orange"]}
+      />
     </div>
   );
 }
